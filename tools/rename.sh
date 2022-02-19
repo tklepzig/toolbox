cb0e836f3c0f1b319bac39e3305225ccb4a1fd20
Thomas <Thomas@DevPad>
2022-02-19 12:45:43 +0100

    Adjust rename

--- tools/rename.sh
+++ tools/rename.sh
@@ -1,6 +1,7 @@
 start=1
-for file in *.mp3; do
-  new=$(printf "%02d.mp3" "$start") #02 pad to length of 2
+ext="mp3"
+for file in *.$ext; do
+  new=$(printf "%02d.$ext" "$start") #02 pad to length of 2
   echo "Renaming $file to $new"
   #mv -i -- "$file" "$new"
   let start=start+1
