f2d289f488eea97a9ef44f73944906c88ff47b18
Thomas Klepzig <thomas.klepzig@ivx.com>
2022-06-21 16:14:27 +0200

    docs

--- docs/writeIndex.sh
+++ docs/writeIndex.sh
@@ -3,5 +3,9 @@
 echo "# Toolbox - Docs" > $(dirname "$0")/index.md
 for file in $(dirname "$0")/*.md
 do
+  if [ "$(basename $file)" = "index.md" ]
+  then
+    continue
+  fi
 echo "- [$(basename "$file" ".md")]($(basename "$file" ))" >> $(dirname "$0")/index.md
 done
