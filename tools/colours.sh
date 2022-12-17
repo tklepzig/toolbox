e55c5370d374fd58cf9de921fa27f89961e43789
Thomas <Thomas@DevPad>
2022-12-17 12:52:37 +0100

    Improve colour script

--- tools/colours.sh
+++ tools/colours.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 for i in {0..255} ; do
-    printf "\x1b[38;5;${i}mcolour${i}  \x1b[48;5;${i}m colour${i} \e[0m\n"
+    name=$(printf "%09s" "colour$i")
+    printf "\x1b[38;5;${i}m$name  \x1b[48;5;${i}m $name \e[0m\t"
+
+    if [ $((($i + 1) % ${1-4})) == 0 ]
+    then
+        printf "\n"
+    fi
 done
